d48822fb3b1f9be19d521c09624f33958a42513f
= <=>
2017-02-27 09:50:56 +0100

    Added PSR instructions, some minor fixes

--- instr.fs
+++ instr.fs
@@ -1,5 +1,4 @@
 //to be merged with types.fsx later
-
 type RegisterName = R0 | R1 | R2 | R3 | R4 | R5| R6 | R7 | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
 
 type ArithLogicOp =
@@ -24,6 +23,16 @@ type TestOp =
 | CMP
 | CMN
 
+type PSROp =
+| MRS
+| MSR
+
+type PSR =
+| APSR
+| CPSR
+| SPSR
+| Mpsr
+
 type ConditionCode =
 | EQ
 | NE
@@ -42,19 +51,6 @@ type ConditionCode =
 | AL
 | NV
 
-type Nibble = uint32 //placeholder
-
-
-type FLexOp =
-| Const of Nibble*byte
-| Shift of byte*RegisterName
-
-
-type ArithLogicInstr = {Cond: ConditionCode; Op: ArithLogicOp; S:bool option; Rd: RegisterName; Rn: RegisterName; Op2: FLexOp}
-type MoveInstr = {Cond: ConditionCode; Op: MoveOp; S:bool option; Rd: RegisterName; Op2: FLexOp}
-type TestInstr = {Cond: ConditionCode; Op: TestOp; Rn: RegisterName; Op2: FLexOp}
-type BranchInstr = {Cond: ConditionCode; L:bool; Address: byte*uint16} //Address type
-
 type ShiftOp =
 | ASR
 | LSL
@@ -62,11 +58,12 @@ type ShiftOp =
 | ROR
 | RRX
 
-type ImReg =
-| Immediate of int
-| Register of RegisterName
-
-type ShiftInstr = {Cond: ConditionCode; Op: ShiftOp; S:bool; Rd: RegisterName; Rn: RegisterName; Param: ImReg option} //Last parameter is option becase RRX only has 2 registers as parameters
+type APSRFlag =
+| N
+| Z
+| C
+| V
+| Q
 
 type MultOp =
 | MUL
@@ -75,4 +72,23 @@ type MultOp =
 | UMULL
 | UMLAL
 
-type MultInstr = {Cond: ConditionCode; Op: MultOp; S:bool option; Rd: RegisterName; Rm: RegisterName; Rs: RegisterName; Rn: RegisterName option} //Mul only has 3 registers as parameters that's why last one is option; MLS cannot have S suffix, therefore it is also option
+type Nibble = uint32 //placeholder
+type Imm8m = int //placeholder, should be a number created by rotating an 8-bit value by an even number of bits within a 32-bit register
+
+type ImReg =
+| Immediate of Imm8m
+| Register of RegisterName
+
+
+type FLexOp =
+| Const of Imm8m
+| Shift of byte*RegisterName
+
+type ArithLogicInstr = {Cond: ConditionCode option; Op: ArithLogicOp; S:bool option; Rd: RegisterName; Rn: RegisterName; Op2: FLexOp}
+type MoveInstr = {Cond: ConditionCode option; Op: MoveOp; S:bool option; Rd: RegisterName; Op2: FLexOp}
+type TestInstr = {Cond: ConditionCode option; Op: TestOp; Rn: RegisterName; Op2: FLexOp}
+type BranchInstr = {Cond: ConditionCode option; L:bool; Address: byte*uint16} //Address type TBD, 24bit field originally
+type MRSInstr = {Cond: ConditionCode option; Rd:RegisterName; Psr:PSR}
+type MSRInstr = {Cond: ConditionCode option; Flags: APSRFlag list ;Param: ImReg}
+type ShiftInstr = {Cond: ConditionCode option; Op: ShiftOp; S:bool; Rd: RegisterName; Rn: RegisterName; Param: ImReg option} //Last parameter is option becase RRX only has 2 registers as parameters
+type MultInstr = {Cond: ConditionCode option; Op: MultOp; S:bool option; Rd: RegisterName; Rm: RegisterName; Rs: RegisterName; Rn: RegisterName option} //Mul only has 3 registers as parameters that's why last one is option; MLS cannot have S suffix, therefore it is also option
