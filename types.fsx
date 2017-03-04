3def7ff07f3060507bd8259fd702c7a9d3c9986c
= <=>
2017-03-04 14:35:36 +0100

    Moved instruction types to types.fsx

--- types.fsx
+++ types.fsx
@@ -1,20 +1,8 @@
 module Types
 
-type Register = int
+type Register = uint32
 type Word = uint32
 
-type Instr =
- | ArithLogicInstr // Flex op2
- | MoveInstr // Flex op2
- | TestInstr // Flex op2
- | MultInstr
- | ShiftInstr
- | BranchInstr
- | PSRInstr
- | MemInstr
- | MiscInstr
-
-type ArithLogicInstr = string*int*byte
 
 type PossiblyDecodedWord =
  | Instr
@@ -45,5 +33,110 @@ type MachineRepresentation = {
     Registers: RegisterFile;
     CPSR: CPSR;
 }
+
+type ArithLogicOp =
+| AND
+| EOR
+| SUB
+| RSB
+| ADD
+| ADC
+| SBC
+| RSC
+| ORR
+| BIC
+
+type MoveOp =
+| MOV
+| MVN
+
+type TestOp =
+| TST
+| TEQ
+| CMP
+| CMN
+
+type PSROp =
+| MRS
+| MSR
+
+type PSR =
+| APSR
+| CPSR
+| SPSR
+| Mpsr
+
+type ConditionCode =
+| EQ
+| NE
+| CS
+| CC
+| MI
+| PL
+| VS
+| VC
+| HI
+| LS
+| GE
+| LT
+| GT
+| LE
+| AL
+| NV
+
+type ShiftOp =
+| ASR
+| LSL
+| LSR
+| ROR
+| RRX
+
+type APSRFlag =
+| N
+| Z
+| C
+| V
+| Q
+
+type MultOp =
+| MUL
+| MLA
+| MLS
+| UMULL
+| UMLAL
+
+type Nibble = byte //placeholder
+type Imm8m = int //placeholder, should be a number created by rotating an 8-bit value by an even number of bits within a 32-bit register
+
+type ImReg =
+| Immediate of Imm8m
+| Register of RegisterName
+
+
+type FLexOp =
+| Const of Imm8m
+| Shift of sbyte*RegisterName
+
+
+type ArithLogicInstr = {Cond: ConditionCode option; Op: ArithLogicOp; S:bool; Rd: RegisterName; Rn: RegisterName; Op2: FLexOp}
+type MoveInstr = {Cond: ConditionCode option; Op: MoveOp; S:bool; Rd: RegisterName; Op2: FLexOp}
+type TestInstr = {Cond: ConditionCode option; Op: TestOp; Rn: RegisterName; Op2: FLexOp}
+type BranchInstr = {Cond: ConditionCode option; L:bool; Address: byte*uint16} //Address type TBD, 24bit field originally
+type MRSInstr = {Cond: ConditionCode option; Rd:RegisterName; Psr:PSR}
+type MSRInstr = {Cond: ConditionCode option; Flags: APSRFlag list ;Param: ImReg}
+type ShiftInstr = {Cond: ConditionCode option; Op: ShiftOp; S:bool; Rd: RegisterName; Rn: RegisterName; Param: ImReg option} //Last parameter is option becase RRX only has 2 registers as parameters
+type MultInstr = {Cond: ConditionCode option; Op: MultOp; S:bool; Rd: RegisterName; Rm: RegisterName; Rs: RegisterName; Rn: RegisterName option} //Mul only has 3 registers as parameters that's why last one is option; MLS cannot have S suffix, therefore it is also option
+
+
+type Instr =
+ | ArithLogicInstr of ArithLogicInstr// Flex op2
+ | MoveInstr of MoveInstr // Flex op2
+ | TestInstr of TestInstr// Flex op2
+ | MultInstr of MultInstr
+ | ShiftInstr of ShiftInstr
+ | BranchInstr of BranchInstr
+ | PSRInstr of MRSInstr | MSRInstr
+ | MemInstr
+ | MiscInstr
 
 // val FrontEnd: string -> (MachineRepresentation Option * FrontendStatusItem list)
