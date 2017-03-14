a01e6d5aa571ffa9f90bfcebd552cecf8e3b464a
= <=>
2017-03-14 16:58:09 +0100

    Added execution function signatures

--- instr.fsx
+++ instr.fsx
@@ -2,12 +2,12 @@ module Instruction
 #load "types.fsx"
 open Types
 
-let fetch machineState =
+let fetch (machineState:MachineRepresentation) : PossiblyDecodedWord =
     let PC = machineState.Registers.[R15] |> uint32
     machineState.Memory.[PC]
 
 
-let secondOp flexOp machineState =
+let secondOp (flexOp:FlexOp) (machineState:MachineRepresentation) : int =
   match flexOp with
   | Const n -> int n
   | Shift (b, Rn) -> machineState.Registers.[Rn] <<< int32 b
@@ -18,11 +18,11 @@ let boolToInt = function
     | false -> 0
 
 
-let writeRegister rd machineState res =
+let writeRegister (rd:RegisterName) (machineState:MachineRepresentation) (res:int):RegisterFile =
   Map.add rd res machineState.Registers
 
 
-let getMemWord (byteAddressing:bool) (address:Address) (machineState:MachineRepresentation) =
+let getMemWord (byteAddressing:bool) (address:Address) (machineState:MachineRepresentation) : Word =
   match byteAddressing with
   | false when address%4u <> 0u -> failwithf "Unaligned memory access"
   | false -> match machineState.Memory.[address] with
@@ -33,7 +33,7 @@ let getMemWord (byteAddressing:bool) (address:Address) (machineState:MachineRepr
              | Instr instr, _ -> failwithf "Data access attempt within instruction space"
 
 
-let storeMemWord (byteAddressing:bool) (address:Address) (word:Word) (machineState:MachineRepresentation) =
+let storeMemWord (byteAddressing:bool) (address:Address) (word:Word) (machineState:MachineRepresentation) : MachineRepresentation =
  match byteAddressing with
  | false when address%4u <> 0u -> failwithf "Unaligned memory access"
  | false -> match machineState.Memory.[address] with
@@ -117,7 +117,7 @@ let getShiftFlags shiftDir f x y =
   res, resFlags
 
 
-let execArithLogicInstr (arithLogicInstr:ArithLogicInstr) (machineState:MachineRepresentation) =
+let execArithLogicInstr (arithLogicInstr:ArithLogicInstr) (machineState:MachineRepresentation) : MachineRepresentation =
     let opMatch  = function
         | AND -> (&&&), getFlags (&&&)
         | EOR -> (^^^), getFlags (^^^)
@@ -139,7 +139,7 @@ let execArithLogicInstr (arithLogicInstr:ArithLogicInstr) (machineState:MachineR
     {machineState with Registers = writeRegister arithLogicInstr.Rd machineState res; CPSR=flags}
 
 
-let execTestInstr (testInstr:TestInstr) (machineState:MachineRepresentation) =
+let execTestInstr (testInstr:TestInstr) (machineState:MachineRepresentation) : MachineRepresentation =
   let opMatch = function
     | TST -> getFlags (&&&)
     | TEQ -> getFlags (^^^)
@@ -150,7 +150,7 @@ let execTestInstr (testInstr:TestInstr) (machineState:MachineRepresentation) =
   {machineState with CPSR=flags}
 
 
-let execMoveInstr (movInstr:MoveInstr) (machineState:MachineRepresentation) =
+let execMoveInstr (movInstr:MoveInstr) (machineState:MachineRepresentation) : MachineRepresentation =
     let opMatch = function
       | MOV -> id, fun x -> (x, {N = (x < 0); Z = (x=0); C=false; V=false;})
       | MVN -> (~~~), fun x -> (~~~x, {N = (~~~x < 0); Z = (~~~x=0); C=false; V=false;})
@@ -164,7 +164,7 @@ let execMoveInstr (movInstr:MoveInstr) (machineState:MachineRepresentation) =
     {machineState with Registers=writeRegister movInstr.Rd machineState res; CPSR = flags}
 
 
-let execShiftInstr (shiftInstr:ShiftInstr) (machineState:MachineRepresentation) =
+let execShiftInstr (shiftInstr:ShiftInstr) (machineState:MachineRepresentation) : MachineRepresentation =
     let rotateRight reg shift =
         let longReg = int64 reg
         let rotated = longReg <<< (32 - shift%32)
@@ -191,7 +191,7 @@ let execShiftInstr (shiftInstr:ShiftInstr) (machineState:MachineRepresentation) 
     {machineState with Registers=writeRegister shiftInstr.Rd machineState res; CPSR=flags}
 
 
-let execBranchInstr (branchInstr:BranchInstr) (machineState:MachineRepresentation) =
+let execBranchInstr (branchInstr:BranchInstr) (machineState:MachineRepresentation) : MachineRepresentation =
   let dest = int branchInstr.Address
   let jumpState = {machineState with Registers = writeRegister R15 machineState dest}
   match branchInstr.L with
@@ -201,7 +201,7 @@ let execBranchInstr (branchInstr:BranchInstr) (machineState:MachineRepresentatio
     {jumpState with Registers = (writeRegister R14 machineState link)}
 
 
-let execSingleMemInstr (memInstr:SingleMemInstr) (machineState:MachineRepresentation) =
+let execSingleMemInstr (memInstr:SingleMemInstr) (machineState:MachineRepresentation) : MachineRepresentation =
   let offset = secondOp memInstr.Offset machineState
   let loadPointer, resPointer =
     match memInstr.Addressing, machineState.Registers.[memInstr.Pointer] with
@@ -216,7 +216,7 @@ let execSingleMemInstr (memInstr:SingleMemInstr) (machineState:MachineRepresenta
   {writtenMem with Registers = writeRegister memInstr.Pointer machineState resPointer}
 
 
-let execMultiMemInstr (memInstr:MultiMemInstr) (machineState:MachineRepresentation) =
+let execMultiMemInstr (memInstr:MultiMemInstr) (machineState:MachineRepresentation) : MachineRepresentation =
   let offset, initPointer =
     match memInstr.Dir with
     | IA -> 4, machineState.Registers.[memInstr.Pointer]
@@ -232,50 +232,50 @@ let execMultiMemInstr (memInstr:MultiMemInstr) (machineState:MachineRepresentati
   | true -> loadedState
   | false -> {loadedState with Registers = writeRegister memInstr.Pointer loadedState machineState.Registers.[memInstr.Pointer]}
 
-let writeRegisters (registers: RegisterFile) machineState =
-    let newMap = Map.fold (fun acc key value -> Map.add key value acc) machineState.Registers registers
-    newMap 
 
-let execMultInstr (multInstr:MultInstr) machineState =
+let writeRegisters (registers: RegisterFile) (machineState:MachineRepresentation) : RegisterFile =
+  Map.fold (fun acc key value -> Map.add key value acc) machineState.Registers registers
+
+
+let execMultInstr (multInstr:MultInstr) (machineState:MachineRepresentation) : MachineRepresentation =
     let myFlags = {N=false; Z=false; C=false; V=false}
-
 
     let umullFun rdHi (rm:Register) (rs:Register) rdLo  =
         let res : uint64 =  (uint64 (uint32 rm)) * (uint64 (uint32 rs))
-        let rdHiVal  = int32 (res >>> 32)  
+        let rdHiVal  = int32 (res >>> 32)
         let rdLoVal  = int32 res
-        [rdLoVal ; rdHiVal] 
+        [rdLoVal ; rdHiVal]
 
     let umlalFun rdHi (rm:Register) (rs:Register) rdLo  =
-        let tmpRes : uint64 = (uint64 (uint32 rdLo )) ||| ( (uint64 (uint32 rdHi )) <<< 32 ) 
+        let tmpRes : uint64 = (uint64 (uint32 rdLo )) ||| ( (uint64 (uint32 rdHi )) <<< 32 )
         let res : uint64 = tmpRes +  (uint64 (uint32 rm)) * (uint64 (uint32 rs))
-        let rdHiVal  = int32 (res >>> 32)  
+        let rdHiVal  = int32 (res >>> 32)
         let rdLoVal  = int32 res
         [rdLoVal ; rdHiVal]
 
     let smullFun rdHi (rm : Register) (rs : Register) rdLo =
         let res : int64 =  (int64  rm) * (int64  rs)
-        let rdHiVal  = int32 (res >>> 32)  
+        let rdHiVal  = int32 (res >>> 32)
         let rdLoVal  = int32 res
         [rdLoVal ; rdHiVal]
 
     let smlalFun rdHi (rm:Register) (rs:Register) rdLo  =
-        let tmpRes : int64 = (int64  rdLo ) ||| ( (int64  rdHi ) <<< 32 ) 
+        let tmpRes : int64 = (int64  rdLo ) ||| ( (int64  rdHi ) <<< 32 )
         let res : int64 = tmpRes +  (int64 rm) * (int64  rs)
-        let rdHiVal  = int32 (res >>> 32)  
+        let rdHiVal  = int32 (res >>> 32)
         let rdLoVal  = int32 res
         [rdLoVal ; rdHiVal]
 
     let opMatch multOp =
         match multOp with
-        | MUL -> (fun a b _ _ -> [a * b]) , [multInstr.Rd] 
+        | MUL -> (fun a b _ _ -> [a * b]) , [multInstr.Rd]
         | MLA -> (fun a b c _ -> [c + a*b]) , [multInstr.Rd]
         | MLS -> (fun a b c _ -> [c - a*b]) , [ multInstr.Rd]
         | UMULL -> (umullFun) , [multInstr.Rd ; multInstr.Rm]
         | UMLAL -> (umlalFun) , [multInstr.Rd ; multInstr.Rm]
         | SMULL -> (smullFun) , [multInstr.Rd ; multInstr.Rm]
         | SMLAL -> (smlalFun) , [multInstr.Rd ; multInstr.Rm]
-    
+
     let multInstrFun = (opMatch multInstr.Op)
     let rd = machineState.Registers.[multInstr.Rd]
     let rm = machineState.Registers.[multInstr.Rm]
@@ -283,14 +283,14 @@ let execMultInstr (multInstr:MultInstr) machineState =
     let rn = match multInstr.Rn with
              | Some registerName ->  machineState.Registers.[registerName]
              | None -> 0
-    
+
     let resValue = fst (opMatch multInstr.Op) rm rs rn rd
     let resReg = snd (opMatch multInstr.Op)
-    let registers =  List.zip resReg resValue |> Map.ofList 
-    {machineState with Registers = writeRegisters registers machineState }                      
+    let registers =  List.zip resReg resValue |> Map.ofList
+    {machineState with Registers = writeRegisters registers machineState }
 
 
-let decode (possiblyInstr:PossiblyDecodedWord) =
+let decode (possiblyInstr:PossiblyDecodedWord) : (unit->MachineRepresentation->MachineRepresentation) =
     match possiblyInstr with
     | Word _ -> failwithf "Word decoded"
     | Instr instr ->
@@ -310,14 +310,14 @@ let decode (possiblyInstr:PossiblyDecodedWord) =
 let execute arg = fun x -> x() arg
 
 
-let pipeLine machineState =
+let pipeLine (machineState:MachineRepresentation) : MachineRepresentation =
   machineState
   |> fetch
   |> decode
   |> execute {machineState with Registers= writeRegister R15 machineState (machineState.Registers.[R15] + 4)}
 
 
-let rec execWrapper machineState:MachineRepresentation =
+let rec execWrapper (machineState:MachineRepresentation) : MachineRepresentation =
   let stoppingCondition = (machineState.Registers.[R15]/4 = List.length (Map.toList machineState.Memory))
   match stoppingCondition with
   | true -> machineState
