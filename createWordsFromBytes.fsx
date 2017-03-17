89da8c358fa926d925f59c9d83045bfa1654acfa
= <=>
2017-03-17 23:14:20 +0100

    Aesthetical changes

--- createWordsFromBytes.fsx
+++ createWordsFromBytes.fsx
@@ -1,7 +1,7 @@
-let rec createWordsFromBytes (myList:int list) = 
+let rec createWordsFromBytes (myList:int list) =
   match myList with
-  | a::b::c::d::t -> a + (b <<< 8) + (c <<< 16) + (d<<<24)::createWordsFromBytes t
-  | a::b::[c] -> [a+(b <<< 8) + (c <<< 16)]
-  | a::[b] ->  [a + (b <<< 8)]
+  | a :: b :: c :: d :: t -> a + (b <<< 8) + (c <<< 16) + (d<<<24) :: createWordsFromBytes t
+  | a :: b :: [c] -> [a + (b <<< 8) + (c <<< 16)]
+  | a :: [b] ->  [a + (b <<< 8)]
   | [a] -> [a]
   | [] -> []
