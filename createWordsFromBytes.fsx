14404dfe925d5015730969985a3d515c8946a09e
= <=>
2017-03-17 23:15:35 +0100

    createWordsFromBytes simplified

--- createWordsFromBytes.fsx
+++ createWordsFromBytes.fsx
@@ -1,5 +1,4 @@
-let rec createWordsFromBytes (myList:int list) =
-  match myList with
+let rec createWordsFromBytes  = function
   | a :: b :: c :: d :: t -> a + (b <<< 8) + (c <<< 16) + (d<<<24) :: createWordsFromBytes t
   | a :: b :: [c] -> [a + (b <<< 8) + (c <<< 16)]
   | a :: [b] ->  [a + (b <<< 8)]
