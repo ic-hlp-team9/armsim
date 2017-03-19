7f74bba9fa33b6c40a83f05705085c2e991f189c
= <=>
2017-03-19 12:51:05 +0100

    Added custom int->hex string function due to FABLE incompatibility

--- checkImm.fs
+++ checkImm.fs
@@ -1,3 +1,13 @@
+let intToHexString n = 
+    let mapToString = function 
+    | 10u -> "A"
+    | 11u -> "B"
+    | 12u -> "C"
+    | 13u -> "D"
+    | 14u -> "E"
+    | 15u -> "F"
+    | x -> string(x)
+    List.fold (fun (myNum, myStr) thisShift -> (myNum, mapToString ((myNum >>> thisShift) &&& 15u) + myStr) ) ((uint32 n), "") [0..4..28] |> snd
 let checkImmediate (num:int) =
   let rotateRight n = int ((uint32 n) >>> 1 ||| ((uint32 n &&& 1u) <<< 31))
   let rec chIm im rotNum valid = 
